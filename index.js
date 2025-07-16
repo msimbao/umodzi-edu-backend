cce0657f6e7bdb89a0074b716dce7a6bcbf697c4
msimbao <mphatsosimbao@gmail.com>
2025-07-16 11:06:32 +0200

    changing a bit of things, hope it works

--- index.js
+++ index.js
@@ -10,7 +10,13 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 // Middleware
-app.use(cors());
+// app.use(cors());
+
+app.use(cors({
+  origin: ['http://localhost:3000', 'umodzi-edu.netlify.app'],
+  credentials: true
+}));
+
 app.use(express.json());
 
 // MTN MoMo API Configuration
